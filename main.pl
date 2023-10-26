50de58bfc5a2aacdd6f4b0dbaa3c5bd268fffe1e
Victor <110508234+victorverissimo6@users.noreply.github.com>
2023-10-26 15:38:48 -0300

    Main do projeto

---
+++ main.pl
@@ -0,0 +1,42 @@
+% Definições de registradores
+reg(a0, 0).
+reg(a1, 0).
+% Adicione mais definições de registradores conforme necessário
+
+% Implementação da instrução addi (adição imediata)
+instrucao(addi, Dest, Src, Immediate) :-
+    reg(Src, Valor),
+    Soma is Valor + Immediate,
+    retract(reg(Dest, _)),
+    assert(reg(Dest, Soma)).
+
+% Implementação da instrução add (adição)
+instrucao(add, Dest, Src1, Src2) :-
+    reg(Src1, Valor1),
+    reg(Src2, Valor2),
+    Soma is Valor1 + Valor2,
+    retract(reg(Dest, _)),
+    assert(reg(Dest, Soma)).
+
+% Implementação da instrução jump (salto incondicional)
+instrucao(jump, Destino) :-
+    retract(program_counter(_)),  % Atualiza o contador de programa
+    assert(program_counter(Destino)).
+
+% Implementação de outras instruções...
+
+% Função principal
+main :-
+    % Instruções do programa
+    instrucao(addi, a0, a0, 3),
+    instrucao(add, a1, a1, a0),
+    instrucao(jump, fim),  % Salto para uma instrução chamada "fim"
+    
+    % Outras instruções do programa...
+
+    halt.
+
+% Definição de um ponto de parada
+halt.
+
+:- main.
